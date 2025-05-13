d4ecf3915b70e559ebb31eb74a832400f42abb67
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2025-05-13 18:23:06 +0530

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,67 @@
-import Image from 'next/image'
-import styles from './page.module.css'
 
-export default function Home() {
+"use client";
+
+import { DynamicWidget } from "@/lib/dynamic";
+import { useState, useEffect } from "react";
+import DynamicMethods from "@/app/components/Methods";
+import { useDarkMode } from "@/lib/useDarkMode";
+import "./page.css";
+import Image from "next/image";
+
+export default function Main() {
+  const { isDarkMode } = useDarkMode();
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className={`container ${isDarkMode ? 'dark' : 'light'}`}>
+      <div className="header">
+        <Image
+          className="logo"
+          src={isDarkMode ? "/logo-light.png" : "/logo-dark.png"}
+          alt="dynamic"
+          width="300"
+          height="60"
+        />{" "}
+        <div className="header-buttons">
+          <button
+            className="docs-button"
+            onClick={() =>
+              window.open(
+                "https://docs.dynamic.xyz",
+                "_blank",
+                "noopener,noreferrer"
+              )
+            }
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Docs
+          </button>
+          <button
+            className="get-started"
+            onClick={() =>
+              window.open(
+                "https://app.dynamic.xyz",
+                "_blank",
+                "noopener,noreferrer"
+              )
+            }
+          >
+            Get started
+          </button>
         </div>
       </div>
-
-      <div className={styles.center}>
+      <div className="modal">
+        <DynamicWidget />
+        <DynamicMethods isDarkMode={isDarkMode} />
+      </div>
+      <div className="footer">
+        <div className="footer-text">Made with 💙 by dynamic</div>
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          className="footer-image"
+          src={isDarkMode ? "/image-dark.png" : "/image-light.png"}
+          alt="dynamic"
+          width="400"
+          height="300"
         />
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
