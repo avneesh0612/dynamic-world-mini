3b7b1f1b4147efb6b606b117b976bdddfbddb45c
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2025-05-31 01:23:13 -0700

    Merge pull request #2 from andy-t-wang/working-web

--- lib/providers.tsx
+++ lib/providers.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { config } from "@/lib/wagmi";
-// import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
-// import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
-// import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
+import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { MiniKitProvider } from "@worldcoin/minikit-js/minikit-provider";
 import { useEffect } from "react";
 import { WagmiProvider } from "wagmi";
 
@@ -17,22 +18,20 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   }, []);
 
   return (
-    // <DynamicContextProvider
-    //   theme="auto"
-    //   settings={{
-    //     environmentId: "9ca34db1-1322-40a5-9991-8eaeaecf7c6d",
-    //     walletConnectors: [EthereumWalletConnectors],
-    //   }}
-    // >
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        {/* <DynamicWagmiConnector> */}
-        {/* <MiniKitProvider> */}
-        {children}
-        {/* </MiniKitProvider> */}
-        {/* </DynamicWagmiConnector> */}
-      </QueryClientProvider>
-    </WagmiProvider>
-    // </DynamicContextProvider>
+    <DynamicContextProvider
+      theme="auto"
+      settings={{
+        environmentId: "9ca34db1-1322-40a5-9991-8eaeaecf7c6d",
+        walletConnectors: [EthereumWalletConnectors],
+      }}
+    >
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <DynamicWagmiConnector>
+            <MiniKitProvider>{children}</MiniKitProvider>
+          </DynamicWagmiConnector>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </DynamicContextProvider>
   );
 }
